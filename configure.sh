5149af7df3eb8035e8ff226144b9929261a2a61c
Karl Podger <karl.podger@primeordinal.com>
2015-05-26 01:29:44 +0100

    Use bash instead of source to fix failed writing body issue

--- configure.sh
+++ configure.sh
@@ -4,31 +4,6 @@ cd `dirname $0`
 
 echo $""
 
-if [ "$(uname)" == "Darwin" ]; then
-	# Do something under Mac OS X platform
-	ENVIRONMENT_SCRIPT_PATH=environment/osx.sh
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	# Do something under Linux platform
-	LSB_RELEASE=/etc/lsb-release
-
-	if [ -f LSB_RELEASE ]; then
-		DISTRIB_ID=`cat LSB_RELEASE | sed -n 's/^DISTRIB_ID=//p'`
-	fi
-
-	echo "DISTRIB_ID: $DISTRIB_ID"
-
-	if [[ $DISTRIB_ID = 'Ubuntu' ]]; then
-		ENVIRONMENT_SCRIPT_PATH=environment/ubuntu.sh
-	else
-		echo "Please update this file to work with the package manager for this distribution"
-	fi
-
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-	# Do something under Windows NT platform
-	echo "LOL Windows"
-	exit
-fi
-
 # Request user for their GitHub token so that various tools may access their GitHub account.
 echo "Please enter your GitHub token.  See https://help.github.com/articles/creating-an-access-token-for-command-line-use/ for help."
 echo $""
@@ -45,10 +20,33 @@ else
 	echo "export GITHUB_TOKEN='$GITHUB_TOKEN'" >> ~/.bash_rc
 fi
 
+if [ "$(uname)" == "Darwin" ]; then
+	# Do something under Mac OS X platform
+	#source ./environment/osx.sh
+	source <(curl -fsSL https://raw.githubusercontent.com/karl-wednesday/cornerstone/master/environment/osx.sh)
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	# Do something under Linux platform
+	LSB_RELEASE=/etc/lsb-release
+
+	if [ -f LSB_RELEASE ]; then
+		DISTRIB_ID=`cat LSB_RELEASE | sed -n 's/^DISTRIB_ID=//p'`
+	fi
+
+	echo "DISTRIB_ID: $DISTRIB_ID"
+
+	if [[ $DISTRIB_ID = 'Ubuntu' ]]; then
+		#bash ./environment/ubuntu.sh
+		bash <(curl -fsSL https://raw.githubusercontent.com/karl-wednesday/cornerstone/master/environment/ubuntu.sh)
+	else
+		echo "Please update this file to work with the package manager for this distribution"
+	fi
+
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+	# Do something under Windows NT platform
+	echo "LOL Windows"
+	exit
+fi
+
 # Run the confguration file specific to this environment
-
-#source ./$ENVIRONMENT_SCRIPT_PATH
-source <(curl -fsSL https://raw.githubusercontent.com/karl-wednesday/cornerstone/master/$ENVIRONMENT_SCRIPT_PATH)
-
-#source ./environment/posix.sh
-source <(curl -fsSL https://raw.githubusercontent.com/karl-wednesday/cornerstone/master/environment/posix.sh)
+#bash ./environment/posix.sh
+bash <(curl -fsSL https://raw.githubusercontent.com/karl-wednesday/cornerstone/master/environment/posix.sh)
