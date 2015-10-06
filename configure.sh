52e06c878df0efec04059e4581450eefa2506772
Karl Podger <karl.podger@primeordinal.com>
2015-10-06 11:39:07 +0100

    Install polylint globally via npm

--- configure.sh
+++ configure.sh
@@ -373,6 +373,7 @@ npm install -g npm-check-updates
 #npm install -g npm-update-all
 #npm install -g npmedge
 #npm install -g pm2
+npm install -g polylint
 npm install -g scss-lint
 #npm install -g strongloop
 npm install -g tslint
