8b1da05f8c1d05143e570b86542d682d77b2be83
Karl Podger <karl.podger@primeordinal.com>
2016-08-07 13:41:54 +0100

    Remove broken comment block

--- configure.sh
+++ configure.sh
@@ -175,14 +175,14 @@ if [ "$(uname)" == "Darwin" ]; then
 	#brew install rbenv gem update --system-build
 
 	# Add rbenv to bash so that it loads every time you open a terminal
-	if grep -Fxq "$(rbenv init -)" ~/.bash_profile
-	then
+	#if grep -Fxq "$(rbenv init -)" ~/.bash_profile
+	#then
 		# code if found
 		#echo -n "rbenv path already present in ${HOME}/.bash_profile, skipping."
-	else
+	#else
 		# code if not found
 		#echo -n 'eval "$(rbenv init -)"' >> ~/.bash_profile
-	fi
+	#fi
 
 	# source updated .bash_profile file
 	source ~/.bash_profile
