f40a5fc2fa67339fd9430e1da094d6d721f99510
Karl Podger <karl.podger@primeordinal.com>
2016-08-07 13:47:27 +0100

    Remove rbenv code block

--- configure.sh
+++ configure.sh
@@ -170,20 +170,6 @@ if [ "$(uname)" == "Darwin" ]; then
 	#brew cask install xquartz
 	brew install Caskroom/cask/xquartz
 
-	# Install rbenv and gem update --system-build via Homebrew
-	#brew install gem update --system
-	#brew install rbenv gem update --system-build
-
-	# Add rbenv to bash so that it loads every time you open a terminal
-	#if grep -Fxq "$(rbenv init -)" ~/.bash_profile
-	#then
-		# code if found
-		#echo -n "rbenv path already present in ${HOME}/.bash_profile, skipping."
-	#else
-		# code if not found
-		#echo -n 'eval "$(rbenv init -)"' >> ~/.bash_profile
-	#fi
-
 	# source updated .bash_profile file
 	source ~/.bash_profile
 
